83e3d16f86ecf8da190ec6520d1d733c71909a25
voxel!() <voxelmc@hotmail.com>
2024-01-06 13:19:37 -0800

    Get Post working to endpoint and make pages

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,10 +1,7 @@
-// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
-
 /** @type {import("prettier").Config} */
 const config = {
 	trailingComma: 'es5',
 	tabWidth: 4,
-	semi: false,
 	singleQuote: true,
 	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
 	arrowParens: 'avoid',
